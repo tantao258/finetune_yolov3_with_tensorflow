16df4ffdec29664baf4675774550ff35cc7e182e
tantao258 <823232162@qq.com>
2018-12-21 22:17:15 -0800

    update

--- core/yolov3.py
+++ core/yolov3.py
@@ -50,7 +50,7 @@ class DarkNet_53(object):
 
 
 class YOLO_V3(object):
-    def __init__(self):
+    def __init__(self, multi_gpu=False, num_gpu=4):
         self.anchors = utils.get_anchors(cfg.anchors_path)
 
         with tf.name_scope("input"):
@@ -67,31 +67,68 @@ class YOLO_V3(object):
             self.boxes_true = [self.y_input_13, self.y_input_26, self.y_input_52]
             self.learning_rate = tf.placeholder(dtype=tf.float32, name="learning_rate")
 
-        with tf.variable_scope('yolov3', reuse=tf.AUTO_REUSE):
-            # train
-            self.feature_maps = forward(self.x_input, self.anchors, is_training=True)
-            # validation
-            self.feature_maps_val = forward(self.x_input, self.anchors, is_training=False)
+        if multi_gpu == False:
+            with tf.variable_scope('yolov3', reuse=tf.AUTO_REUSE):
+                # train
+                self.feature_maps = forward(self.x_input, self.anchors, is_training=True)
+                # validation
+                self.feature_maps_val = forward(self.x_input, self.anchors, is_training=False)
 
-        with tf.variable_scope("loss"):
-            self.loss = compute_loss(self.feature_maps, self.boxes_true, self.anchors)
-            self.loss_coord = self.loss[0]
-            self.loss_sizes = self.loss[1]
-            self.loss_confs = self.loss[2]
-            self.loss_class = self.loss[3]
+            with tf.variable_scope("loss"):
+                self.loss = compute_loss(self.feature_maps, self.boxes_true, self.anchors)
+                self.loss_coord = self.loss[0]
+                self.loss_sizes = self.loss[1]
+                self.loss_confs = self.loss[2]
+                self.loss_class = self.loss[3]
 
-        with tf.variable_scope("train"):
-            self.global_step = tf.Variable(0, name="global_step", trainable=False)
-            update_ops = tf.get_collection(tf.GraphKeys.UPDATE_OPS)
+            with tf.variable_scope("train"):
+                self.global_step = tf.Variable(0, name="global_step", trainable=False)
+                update_ops = tf.get_collection(tf.GraphKeys.UPDATE_OPS)
 
-            var_list = [v for v in tf.trainable_variables()]
-            gradients = tf.gradients(sum(self.loss), var_list)
-            self.grads_and_vars = list(zip(gradients, var_list))
-            optimizer = tf.train.AdadeltaOptimizer(self.learning_rate)
+                var_list = [v for v in tf.trainable_variables()]
+                gradients = tf.gradients(sum(self.loss), var_list)
+                self.grads_and_vars = list(zip(gradients, var_list))
+                optimizer = tf.train.AdadeltaOptimizer(self.learning_rate)
 
-            with tf.control_dependencies(update_ops):
-                self.train_op = optimizer.apply_gradients(grads_and_vars=self.grads_and_vars,
-                                                          global_step=self.global_step)
+                with tf.control_dependencies(update_ops):
+                    self.train_op = optimizer.apply_gradients(grads_and_vars=self.grads_and_vars,
+                                                              global_step=self.global_step)
 
-        with tf.name_scope("prediction"):
-            self.prediction = predict(self.feature_maps_val, self.anchors)
+            with tf.name_scope("prediction"):
+                self.prediction = predict(self.feature_maps_val, self.anchors)
+
+        else:
+            with tf.variable_scope("train"):
+                self.batch_size = tf.shape(self.x_input)[0] // num_gpu
+                with tf.variable_scope(tf.get_variable_scope()):
+                    self.global_step = tf.Variable(0, name="global_step", trainable=False)
+                    update_ops = tf.get_collection(tf.GraphKeys.UPDATE_OPS)
+                    var_list = [v for v in tf.trainable_variables()]
+
+                    optimizer = tf.train.AdadeltaOptimizer(self.learning_rate)
+
+                    tower_grads = []
+                    loss_list = []
+                    for i in range(num_gpu):
+                        with tf.device("/gpu:%d" % i):
+                            with tf.name_scope("tower_%d" % i):
+                                x_input = self.x_input[i * self.batch_size:(i + 1) * self.batch_size]
+                                boxes_true = self.boxes_true[i * self.batch_size:(i + 1) * self.batch_size]
+                                feature_maps = forward(x_input, self.anchors, is_training=True)
+                                loss = compute_loss(feature_maps, boxes_true, self.anchors)
+
+                                gradients = optimizer.compute_gradients(sum(loss), var_list)
+                                tower_grads.append(gradients)
+                                loss_list.append(loss)
+
+                    self.loss = tf.reduce_mean(loss_list, axis=0)
+                    self.loss_coord = self.loss[0]
+                    self.loss_sizes = self.loss[1]
+                    self.loss_confs = self.loss[2]
+                    self.loss_class = self.loss[3]
+
+                    gradients = utils.average_gradients(tower_grads)
+                    self.grads_and_vars = list(zip(gradients, var_list))
+                    with tf.control_dependencies(update_ops):
+                        self.train_op = optimizer.apply_gradients(grads_and_vars=self.grads_and_vars,
+                                                                  global_step=self.global_step)
